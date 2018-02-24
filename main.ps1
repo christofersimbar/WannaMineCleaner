a7c55b65215dbc999087f36bbd91d47e3ee654d8
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-24 14:50:17 +0700

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -11,14 +11,14 @@ foreach($namaserver in Get-Content .\daftarserver.txt) {
   Invoke-Command -ComputerName $namaserver {Remove-WmiObject -Namespace root\default -Class Win32_Services}
   
   #remove malicious __EventFilter instance
-  #change 'DSM Event Logs Filter' to match your environment
-Invoke-Command -ComputerName $namaserver {Get-WmiObject __EventFilter -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Logs Filter'} | Remove-WmiObject}
+  #change 'DSM Event Log Filter' to match your environment
+Invoke-Command -ComputerName $namaserver {Get-WmiObject __EventFilter -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Log Filter'} | Remove-WmiObject}
 
-  #remove malicious __CommandLineEventConsumer instance
-  #change 'DSM Event Logs Consumer' to match your environment
-Invoke-Command -ComputerName $namaserver {Get-WmiObject CommandLineEventConsumer -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Logs Consumer'} | Remove-WmiObject}
+  #remove malicious CommandLineEventConsumer instance
+  #change 'DSM Event Log Consumer' to match your environment
+Invoke-Command -ComputerName $namaserver {Get-WmiObject CommandLineEventConsumer -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Log Consumer'} | Remove-WmiObject}
 
   #remove malicious __FilterToConsumerBinding instance
-  #change 'DSM Event Logs Consumer' to match your environment
-  Invoke-Command -ComputerName $namaserver {Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription | Where-Object {$_.filter -match 'DSM Event Logs Consumer'} | Remove-WmiObject}
+  #change 'DSM Event Log Filter' to match your environment
+  Invoke-Command -ComputerName $namaserver {Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription | Where-Object {$_.filter -match 'DSM Event Log Filter'} | Remove-WmiObject}
 }
