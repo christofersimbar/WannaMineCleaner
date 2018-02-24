1c51824ba6513a1988e4b0d85a602adf00a11265
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-24 14:49:00 +0700

    Update remove_evil_wmi_local.ps1

--- remove_evil_wmi_local.ps1
+++ remove_evil_wmi_local.ps1
@@ -17,11 +17,11 @@ foreach($server in Get-Content .\daftarserver.txt) {
   Invoke-Command -ComputerName $server -credential $credential {Remove-WmiObject -Namespace root\default -Class Win32_Services}
   
   #remove malicious __FilterToConsumerBinding instance
-  Invoke-Command -ComputerName $server -credential $credential {Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription | Where-Object {$_.filter -match 'DSM Event Logs Filter'} | Remove-WmiObject}
+  Invoke-Command -ComputerName $server -credential $credential {Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription | Where-Object {$_.filter -match 'DSM Event'} | Remove-WmiObject}
 
   #remove malicious __EventFilter instance
-Invoke-Command -ComputerName $server -credential $credential {Get-WmiObject __EventFilter -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Logs Consumer'} | Remove-WmiObject}
+Invoke-Command -ComputerName $server -credential $credential {Get-WmiObject __EventFilter -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event'} | Remove-WmiObject}
 
   #remove malicious __CommandLineEventConsumer instance
-Invoke-Command -ComputerName $server -credential $credential {Get-WmiObject CommandLineEventConsumer -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Logs Consumer'} | Remove-WmiObject}
+Invoke-Command -ComputerName $server -credential $credential {Get-WmiObject CommandLineEventConsumer -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event'} | Remove-WmiObject}
 }
