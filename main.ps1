ea502e070d0a1a7279032a8e280a963c0c1a663a
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-24 14:11:50 +0700

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -4,21 +4,21 @@ foreach($namaserver in Get-Content .\daftarserver.txt) {
   
   #kill powershell process, to prevent malware creates new WMI class
   #WARNING! this command will also kill legitimate powershell process
-  Invoke-Command -ComputerName $namaserver {stop-process -Name powershell.exe}
+  #Invoke-Command -ComputerName $namaserver {stop-process -Name powershell.exe}
   
   #remove malicious WMI class
   #change 'Win32_Services' to match your environment
   Invoke-Command -ComputerName $namaserver {Remove-WmiObject -Namespace root\default -Class Win32_Services}
   
   #remove malicious __EventFilter instance
-  #change 'DSM Event' to match your environment
-Invoke-Command -ComputerName $namaserver {Get-WmiObject __EventFilter -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Log Filter'} | Remove-WmiObject}
+  #change 'DSM Event Logs Filter' to match your environment
+Invoke-Command -ComputerName $namaserver {Get-WmiObject __EventFilter -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Logs Filter'} | Remove-WmiObject}
 
   #remove malicious __CommandLineEventConsumer instance
-  #change 'DSM Event' to match your environment
-Invoke-Command -ComputerName $namaserver {Get-WmiObject CommandLineEventConsumer -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Log Consumer'} | Remove-WmiObject}
+  #change 'DSM Event Logs Consumer' to match your environment
+Invoke-Command -ComputerName $namaserver {Get-WmiObject CommandLineEventConsumer -Namespace root\subscription | Where-Object {$_.name -match 'DSM Event Logs Consumer'} | Remove-WmiObject}
 
   #remove malicious __FilterToConsumerBinding instance
-  #change 'DSM Event' to match your environment
-  Invoke-Command -ComputerName $namaserver {Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription | Where-Object {$_.filter -match 'DSM Event Log Consumer'} | Remove-WmiObject}
+  #change 'DSM Event Logs Consumer' to match your environment
+  Invoke-Command -ComputerName $namaserver {Get-WmiObject __FilterToConsumerBinding -Namespace root\subscription | Where-Object {$_.filter -match 'DSM Event Logs Consumer'} | Remove-WmiObject}
 }
