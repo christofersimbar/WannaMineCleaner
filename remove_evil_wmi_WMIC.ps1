9f13f6e02a2752b72633bd3ad7aeb94e17d93b10
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-27 12:07:58 +0700

    Update remove_evil_wmi_WMIC.ps1

--- remove_evil_wmi_WMIC.ps1
+++ remove_evil_wmi_WMIC.ps1
@@ -1,9 +1,13 @@
 foreach($servername in Get-Content .\daftarserver.txt) {
-  #Fill all target hostname without domain name in daftarserver.txt
+
   echo "==================================="
   echo "Processing $servername ..."
   echo "==================================="
+  
+  #these lines are used to kill malicious process which can be identified by their command line
   wmic /node:$servername process WHERE "COMMANDLINE LIKE '%Win32_Services%'" CALL TERMINATE
+  wmic /node:$servername process WHERE "COMMANDLINE LIKE '%info6.ps1%'" CALL TERMINATE
+
   wmic /node:$servername /NAMESPACE:"\\root\default" PATH Win32_Service DELETE
   wmic /node:$servername /NAMESPACE:"\\root\subscription" PATH __EventFilter WHERE "Name LIKE 'DSM Event%'" DELETE
   wmic /node:$servername /NAMESPACE:"\\root\subscription" PATH CommandLineEventConsumer WHERE "Name LIKE 'DSM Event%'" DELETE
