86f6be23257a5553b7df2487848877fbbf37aa6b
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-27 15:44:06 +0700

    Update remove_evil_wmi_WMIC.ps1

--- remove_evil_wmi_WMIC.ps1
+++ remove_evil_wmi_WMIC.ps1
@@ -8,7 +8,7 @@ foreach($ip in Get-Content .\serverlist.txt) {
   wmic /node:$ip process WHERE "COMMANDLINE LIKE '%default:Win32_Services%'" CALL TERMINATE
   wmic /node:$ip process WHERE "COMMANDLINE LIKE '%info6.ps1%'" CALL TERMINATE
 
-  wmic /node:$ip /NAMESPACE:"\\root\default" PATH Win32_Service DELETE
+  wmic /node:$ip /NAMESPACE:"\\root\default" PATH Win32_Services DELETE
   wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __EventFilter WHERE "Name LIKE 'DSM Event%'" DELETE
   wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH CommandLineEventConsumer WHERE "Name LIKE 'DSM Event%'" DELETE
   wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __FilterToConsumerBinding WHERE "Filter=""__EventFilter.Name='DSM Event Log Filter'""" DELETE
