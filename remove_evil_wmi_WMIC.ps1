cb32324247e1f317aaaddabd789f3746adda5200
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-27 11:59:48 +0700

    Update remove_evil_wmi_WMIC.ps1

--- remove_evil_wmi_WMIC.ps1
+++ remove_evil_wmi_WMIC.ps1
@@ -1,7 +1,10 @@
 foreach($servername in Get-Content .\daftarserver.txt) {
+  #Fill all target hostname without domain name in daftarserver.txt
   echo "==================================="
   echo "Processing $servername ..."
   echo "==================================="
+  #uncomment below line to kill powershell, but this action will also kill legitimate powershell process
+  #wmic /node:$servername process where "Name='Powershell.exe'" DELETE
   wmic /node:$servername process where "Name='WmiPrvSE.exe'" DELETE
   wmic /node:$servername /NAMESPACE:"\\root\default" PATH Win32_Service DELETE
   wmic /node:$servername /NAMESPACE:"\\root\subscription" PATH __EventFilter WHERE "Name LIKE 'DSM Event%'" DELETE
