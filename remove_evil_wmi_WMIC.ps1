ab91053664fe553faa2d31a278568691867b8101
Christofer Simbar <christofersimbar@yahoo.com>
2018-02-27 14:05:35 +0700

    Update remove_evil_wmi_WMIC.ps1

--- remove_evil_wmi_WMIC.ps1
+++ remove_evil_wmi_WMIC.ps1
@@ -1,15 +1,15 @@
-foreach($servername in Get-Content .\daftarserver.txt) {
+foreach($ip in Get-Content .\serverlist.txt) {
 
   echo "==================================="
-  echo "Processing $servername ..."
+  echo "Processing $ip ..."
   echo "==================================="
   
   #these lines are used to kill malicious process which can be identified by their command line
-  wmic /node:$servername process WHERE "COMMANDLINE LIKE '%default:Win32_Services%'" CALL TERMINATE
-  wmic /node:$servername process WHERE "COMMANDLINE LIKE '%info6.ps1%'" CALL TERMINATE
+  wmic /node:$ip process WHERE "COMMANDLINE LIKE '%default:Win32_Services%'" CALL TERMINATE
+  wmic /node:$ip process WHERE "COMMANDLINE LIKE '%info6.ps1%'" CALL TERMINATE
 
-  wmic /node:$servername /NAMESPACE:"\\root\default" PATH Win32_Service DELETE
-  wmic /node:$servername /NAMESPACE:"\\root\subscription" PATH __EventFilter WHERE "Name LIKE 'DSM Event%'" DELETE
-  wmic /node:$servername /NAMESPACE:"\\root\subscription" PATH CommandLineEventConsumer WHERE "Name LIKE 'DSM Event%'" DELETE
-  wmic /node:$servername /NAMESPACE:"\\root\subscription" PATH __FilterToConsumerBinding WHERE "Filter LIKE 'DSM Event%'" DELETE
+  wmic /node:$ip /NAMESPACE:"\\root\default" PATH Win32_Service DELETE
+  wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __EventFilter WHERE "Name LIKE 'DSM Event%'" DELETE
+  wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH CommandLineEventConsumer WHERE "Name LIKE 'DSM Event%'" DELETE
+  wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __FilterToConsumerBinding WHERE "Filter LIKE 'DSM Event%'" DELETE
 }
