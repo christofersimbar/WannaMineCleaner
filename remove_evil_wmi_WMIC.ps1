29aefb47b4261a627cb532e95c09f786dd5a9672
Christofer Simbar <christofersimbar@yahoo.com>
2018-04-07 14:37:47 +0700

    Bug Fix: Fail to delete malicious class

--- remove_evil_wmi_WMIC.ps1
+++ remove_evil_wmi_WMIC.ps1
@@ -1,7 +1,7 @@
+write-output "" | out-file "failed.txt"
 foreach($ip in Get-Content .\daftarserver.txt) {
   Write-Output "===================================" 
   Write-Output "Processing $ip ..." 
-  Write-Output "==================================="  
   
   $wmi = gwmi win32_bios -ComputerName $ip -ErrorAction SilentlyContinue
 
@@ -13,21 +13,27 @@ foreach($ip in Get-Content .\daftarserver.txt) {
       wmic /node:$ip process WHERE "COMMANDLINE LIKE '%default:Win32_Services%'" CALL TERMINATE
       wmic /node:$ip process WHERE "COMMANDLINE LIKE '%info6.ps1%'" CALL TERMINATE
       wmic /node:$ip process WHERE "ExecutablePath='C:\\ProgramData\\UpdateService.exe'" CALL TERMINATE
+      wmic /node:$ip process WHERE "ExecutablePath='C:\\ProgramData\\AppCache\\18\\java.exe'" CALL TERMINATE
+      wmic /node:$ip process WHERE "ExecutablePath='C:\\ProgramData\\AppCache\\17\\java.exe'" CALL TERMINATE
       wmic /node:$ip process WHERE "ExecutablePath='C:\\ProgramData\\AppCache\\17_\\java.exe'" CALL TERMINATE
       wmic /node:$ip process WHERE "ExecutablePath='C:\\ProgramData\\AppCache\\16\\java.exe'" CALL TERMINATE
       wmic /node:$ip process WHERE "COMMANDLINE LIKE '%JABzAHQAaQBtAGUAPQBbAEUAbgB2AGkAcgBvAG4AbQBlAG4AdABdADoAOgBUAG%'" CALL TERMINATE
 
       #delete all malicious files
       WMIC /node:$ip path cim_datafile WHERE "path='C:\\ProgramData\\UpdateService.exe'" delete
+      WMIC /node:$ip path cim_datafile WHERE "path='C:\\ProgramData\\AppCache\\18\\java.exe'" delete
+      WMIC /node:$ip path cim_datafile WHERE "path='C:\\ProgramData\\AppCache\\17\\java.exe'" delete
       WMIC /node:$ip path cim_datafile WHERE "path='C:\\ProgramData\\AppCache\\17_\\java.exe'" delete
       WMIC /node:$ip path cim_datafile WHERE "path='C:\\ProgramData\\AppCache\\16\\java.exe'" delete
 
       #change "Win32_Services" and "DSM Event" to match evil class and instance name found in your environment
-      wmic /node:$ip /NAMESPACE:"\\root\default" PATH Win32_Services DELETE
+      wmic /node:$ip /NAMESPACE:"\\root\default" Class Win32_Services DELETE
       wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __EventFilter WHERE "Name LIKE 'DSM Event%'" DELETE
       wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH CommandLineEventConsumer WHERE "Name LIKE 'DSM Event%'" DELETE
       wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __FilterToConsumerBinding WHERE "Filter=""__EventFilter.Name='DSM Event Log Filter'""" DELETE
       wmic /node:$ip /NAMESPACE:"\\root\subscription" PATH __FilterToConsumerBinding WHERE "Filter=""__EventFilter.Name='DSM Event Logs Filter'""" DELETE
 
-  } else {Write-Host "Computer $ip RPC error" -ForegroundColor DarkRed;}
+  } else {
+      write-output "$ip RPC error" | out-file -append "failed.txt";
+  }
 }
